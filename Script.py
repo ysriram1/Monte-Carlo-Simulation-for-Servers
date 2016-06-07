514d8db1b6dd75cd2e95cc0988535da895925295
unknown <SYARLAG1@400LAB34.cstcis.cti.depaul.edu>
2016-06-06 20:39:23 -0500

    updated the bootstrapping function

--- Script.py
+++ Script.py
@@ -44,23 +44,39 @@ def sim_once(time = 60, n = 1000): #simulate for a minute
 
 #We simulate the process many times and report the 95% CI using bootstraping
 
-def sim_many(number = 100, time = 60, n = 100, bootstrapCI = False): #We repeat the above simulation a specified number of times
+def sim_many(number = 100, time = 60, nodes = 100, bootstrapCI = False): #We repeat the above simulation a specified number of times
     '''This function repeats the simulate_once() function a specified number of 
     times and returns the 95% CI of the number of successes, profit, and requests
     via bootstrapping.
     '''    
-    allResults =[sim_once(time = time, n = n) for i in range(number)]
-    profitValues = [x[0] for x in allResults]
-    successes = [x[1] for x in allResults]
-    requests = [x[3] for x in allResults]
+    allResults =[sim_once(time = time, n = nodes) for i in range(number)]
+
+    def mean(S): return float(sum(x for x in S))/len(S)
+    def resample(S): return [random.choice(S) for i in xrange(len(S))]
+        
+    profitValues = [x[0] for x in allResults] 
+    profitValues_rs = [mean(resample(profitValues)) for i in range(len(profitValues))]
+    
+    successes = [float(x[1]) for x in allResults]
+    #successes_rs = [mean(resample(successes)) for i in range(len(successes))]
+    
+    requests = [float(x[3]) for x in allResults]
+    #requests_rs = [mean(resample(requests)) for i in range(len(requests))]
+    
+    request_success = list(np.array(successes)/np.array(requests))
+    request_success_rs = [mean(resample(request_success)) for i in range(len(request_success))]
+
     meanSuccesses = np.mean(successes)
     meanRequests = np.mean(requests)
     meanProfit = np.mean(profitValues) #the is the average profit
     if bootstrapCI: #95% CI via bootstraping
-        profit_05, profit_95 = np.sort(profitValues)[int(0.05*len(profitValues))], profitValues[int(0.95*len(profitValues))]
-        success_05, success_95  = np.sort(successes)[int(0.05*len(successes))], successes[int(0.95*len(successes))]
-        requests_05, requests_95  = np.sort(requests)[int(0.05*len(requests))], requests[int(0.95*len(requests))]
-        return profit_05, profit_95, success_05, success_95, requests_05, requests_95
+        profit_025, profit_975 = np.sort(profitValues_rs)[int(0.025*len(profitValues_rs))], np.sort(profitValues_rs)[int(0.975*len(profitValues_rs))]
+        #success_025, success_975  = np.sort(successes_rs)[int(0.025*len(successes_rs))], np.sort(successes_rs)[int(0.975*len(successes_rs))]
+        #requests_025, requests_975  = np.sort(requests_rs)[int(0.025*len(requests_rs))], np.sort(requests_rs)[int(0.975*len(requests_rs))]
+        request_success_025  = np.sort(request_success_rs)[int(0.025*len(request_success_rs))] 
+        request_success_975 = np.sort(request_success_rs)[int(0.975*len(request_success_rs))]
+
+        return profit_025, profit_975, request_success_025, request_success_975
     else:
         return meanProfit, meanSuccesses, meanRequests 
 
@@ -75,22 +91,21 @@ def plots_profit_success(minN = 10, maxN = 1000, iterations = 1, time = 60):
     used.
     '''    
     
-    nodes = list(range(minN,maxN,10))
+    node_count = list(range(minN,maxN,10))
     results= []
     count = 0
-    for node in nodes:
+    for node in node_count:
         count += 1
-        #print(node)    
-        results.append(sim_many(number = iterations, time = time, n = node))
+        results.append(sim_many(number = iterations, time = time, nodes = node))
     
     results = np.array(results)
     plt.figure()
-    plt.scatter(np.array(nodes), results[:,0])
+    plt.scatter(np.array(node_count), results[:,0])
     plt.title('Processing time of %i seconds (profit earned)'%time); plt.xlabel('Number of Servers'); plt.ylabel('Profit($)')
     plt.xlim(0); plt.show()
         
     plt.figure()
-    plt.scatter(np.array(nodes),100*(results[:,1]/results[:,2]))
+    plt.scatter(np.array(node_count),100*(results[:,1]/results[:,2]))#NOTE: dividing here or in the sim_many function itself wont make a difference if we are constructing graphs with only one iteration
     plt.title('Processing time of %i seconds (requests processed)'%time); plt.xlabel('Number of Servers'); plt.ylabel('% of requests processed')
     plt.xlim(0); plt.ylim(0);plt.show()
 
@@ -101,11 +116,11 @@ for time in range(10, 250, 40): #This will generate the necessary graphs for dif
     plots_profit_success(time = time)
 
 
-#This will generate the 95% CIs via bootstrapping for a 60 seconds simulation with 330 nodes  
-sim_many(number = 100, time = 60, n = 330, bootstrapCI = True)
+#This will generate the 95% CIs via bootstrapping for a 60 seconds simulation with 330 nodes. For max profit
+sim_many(number = 100, time = 60, nodes = 330, bootstrapCI = True)
 
-#This will generate the 95% CIs via bootstrapping for a 60 seconds simulation with 290 nodes  
-sim_many(number = 100, time = 60, n = 330, bootstrapCI = True)
+#This will generate the 95% CIs via bootstrapping for a 60 seconds simulation with 290 nodes. For breakeven 
+sim_many(number = 100, time = 60, nodes = 286, bootstrapCI = True)
 
-#This will generate the 95% CIs via bootstrapping for a 60 seconds simulation with 285 nodes  
-sim_many(number = 100, time = 60, n = 330, bootstrapCI = True)
+#This will generate the 95% CIs via bootstrapping for a 60 seconds simulation with 285 nodes. For 90% success  
+sim_many(number = 100, time = 60, nodes = 275, bootstrapCI = True)
